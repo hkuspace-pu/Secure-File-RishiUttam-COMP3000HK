3a61067d23c5d70de693540f54944d1645fff664
rishi@pmgrouphk.com <rishi@pmgrouphk.com>
2024-04-04 23:58:46 +0800

    wip:Working on middleware function to encrypt/decrypt in the browser

---
+++ crypto-middleware/index.js
@@ -0,0 +1,17 @@
+async function encryptFile(file) {
+
+    // Encrypt the file
+    const encryptedFile = await encrypt(file);
+
+    // Return the encrypted file
+    return encryptedFile;
+
+}
+
+async function encrypt(file) {
+    
+        // Encrypt the file
+        return file;
+}
+
+
