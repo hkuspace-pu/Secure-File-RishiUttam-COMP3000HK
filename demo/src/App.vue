b01f54562f27131a23ce1c19db60f1ae8e7983cd
rishi@pmgrouphk.com <rishi@pmgrouphk.com>
2024-05-07 16:20:19 +0800

    Fix : Graphs showing memory and duration

--- demo/src/App.vue
+++ demo/src/App.vue
@@ -1,30 +1,33 @@
 <template>
   <div class="container">
-    <h1 class="text-3xl font-bold underline">Web crypto demo</h1>
-    <div>
-    <p>Used JS heap size: {{ prettyBytes(memInfo.usedJSHeapSize) }}</p>
-     <p v-show="highestMem">Max memory: {{ prettyBytes(highestMem) }}</p>
-  </div>
+    <!-- <h2 class="text-2xl font-bold underline">Secure Send - Demonstration</h2> -->
+ 
 
 
   <apexchart type="line" height="350"  ref="chart" :options="chartOptions" :series="series"></apexchart>
 
+  
+
 <div class="boxContainer">
     <div class="box">
-      <h2>Encrypt</h2>
-      <input ref="fileInput" id="fileInput" type="file" />
+      <h3>Secure Send - Encrypt</h3>
+      <!-- <input ref="fileInput" id="fileInput" type="file" /> -->
+<FileUpload mode="basic" name="demo[]"  ref="fileInput" id="fileInput" />
+
 
       <input v-model="passphrase" type="text" id="passphrase" placeholder="Enter passphrase" />
-      
+      <button @click="uploadOnly" id="encryptButton">Upload Only</button>
       <button @click="encrypt" id="encryptButton">Encrypt Stream</button>
       <button @click="encryptBuffer" id="encryptButton">Encrypt Buffer</button>
+      <button @click="openPGP" id="encryptButton">OpenPGP</button>
+            <button @click="s3encrypt" id="encryptButton">AS33</button>
    
-      <div class="box">
-      <p>AWS Progress</p>
+      <!-- <div class="box"> -->
+      <!-- <p>AWS Progress</p> -->
       <ProgressBar :value="awsProgress"></ProgressBar>
       <!-- <progress ref="awsProgress" id="awsProgress" max="100" value="0"></progress> -->
      
-    </div>
+    <!-- </div> -->
    
     </div>
      <a ref="downloadLink" id="downloadLink" style="display: none;">Download file</a>
@@ -38,8 +41,17 @@
 
       <a id="downloadLink" style="display: none;">Download file</a>
     </div> -->
+    <div class="box">
+        <apexchart type="radialBar" height="350" :options="chartOptions" :series="radialSeries"></apexchart>
+      </div>
+    <div class="box">
+    <p>Used JS heap size: {{ prettyBytes(memInfo.usedJSHeapSize) }}</p>
+     <p v-show="highestMem">Max memory: {{ prettyBytes(highestMem) }}</p>
+     <p>{{timers.data}}</p>
 
+     <apexchart type="bar" height="350" ref="timersBar" :options="barChartTimerOptions" :series="timers"></apexchart>
 
+  </div>
    
 
     </div>
@@ -84,7 +96,8 @@ import * as openpgp from 'openpgp';
 import { S3Client, ListObjectsV2Command,GetObjectCommand, PutObjectCommand } from "@aws-sdk/client-s3";
 import { Upload } from "@aws-sdk/lib-storage";
 import prettyBytes from 'pretty-bytes'
-
+import FileUpload from 'primevue/fileupload';
+// import * as awscrypto from '@aws-crypto/client-browser'
 
 const client = new S3Client({ region: "ap-east-1",  credentials: {
     accessKeyId: 'AKIAR3HSJBS3T6F7OSEL', 
@@ -104,6 +117,7 @@ let passphrase = ref('');
 let downloadLink = ref(null);
 let awsProgress = ref(0);
 let highestMem = ref(0)
+
 const data = ref(null);
 const memInfo = ref({
       jsHeapSizeLimit: 0,
@@ -112,6 +126,31 @@ const memInfo = ref({
     });
 
     const chart = ref();
+
+
+    const barChartTimerOptions = ref({
+      chart: {
+        type: 'bar',
+        height: 340
+      }, xaxis: {
+              categories: ['Baseline', 'OPENPGP', 'SecureSend'],
+    },yaxis : {
+     title : {
+      text: 'Duration (ms)'
+     }
+    },  title: {
+              text: 'Time taken (ms)',
+              align: 'left'
+            },
+            subtitle : {
+              text: 'Lower is better'
+              
+            }
+  }
+    )
+    
+
+
     const chartOptions = ref({
             chart: {
               id: 'realtime',
@@ -185,17 +224,71 @@ const memInfo = ref({
         }
       ]);
 
+      const timers = ref([
+        {
+          name: 'Timers',
+          data: []
+          // data: traffic2.value.slice()
+       
+        }
+      ]);
+
+
+      const radialSeries = ref([0])
+  
+
 // let downloadLink
 
-const openEncrypt = async () => {
-  console.log('OPEN  PGP TEST')
-const open = await openpgp.generateKey({ curve: 'brainpoolP512r1',  userIDs: [{ name: 'Test', email: 'test@test.com' }] });
-console.log(open)
+const uploadOnly = async () => {
+  console.log('Testing upload perofmrance only')
+  const startTime = performance.now(); 
+  const file = fileInput.value.files[0];
+    const totalBytes = file.size
+    const fileName = file.name
+    // let fileBuffer = await file.arrayBuffer()
+    // const uint8Array = new Uint8Array(fileBuffer);
+  const result = await uploadS3(fileName,totalBytes ,file)
+  console.log('done upload only: ', file)
+  const endTime = performance.now(); // Capture end time
+    console.log(`Upload Only Baseline ${endTime - startTime} milliseconds.`)
+    timers.value[0].data.push(parseFloat((endTime - startTime).toFixed(2)))
 }
 
-// openEncrypt()
 
-//create a method called encrypt
+const openPGP = async () => {
+  console.log('OPEN  PGP TEST')
+  const startTime = performance.now(); 
+   const file = fileInput.value.files[0];
+    const totalBytes = file.size
+    const fileName = file.name
+   let fileBuffer = await file.arrayBuffer()
+
+    const message = await openpgp.createMessage({ binary: new Uint8Array(fileBuffer) });
+const encrypted = await openpgp.encrypt({
+ message:message, // input as Message object
+        passwords: ['secret stuff'], // multiple passwords possible
+        format: 'binary' // don't ASCII armor (for Uint8Array output)
+    });
+    
+
+
+const result = await uploadS3(fileName,totalBytes ,encrypted, 0)
+console.log('finished pgp encrypt')
+const endTime = performance.now(); // Capture end time
+    console.log(`OPEN PGP Encryption took ${endTime - startTime} milliseconds.`);
+    timers.value[0].data.push(parseFloat((endTime - startTime).toFixed(2)))
+
+}
+
+
+
+const s3encrypt = async () => {
+  const plainText = new Uint8Array([1, 2, 3, 4, 5])
+  // const { result } = await awscrypto.encrypt('1234', plainText, { encryptionContext: context })
+console.log(result)
+}
+
+
 
 const updateMemoryInfo = () => {
 
@@ -214,7 +307,7 @@ if (max > highestMem.value) {
 if (max > 100000000) {
   chart.value.updateOptions({
           yaxis: {
-            max: max,
+            max: max + 10000000,
             labels :{
                 formatter: function(val) {
                   return prettyBytes(val)
@@ -238,21 +331,22 @@ series.value[0].data.push([adjustedNow,memInfo.value.usedJSHeapSize]);
 
 const encrypt = async () => {
 try {
-
+  const startTime = performance.now(); 
     const file = fileInput.value.files[0];
     const totalBytes = file.size
 console.log('ENCRYPT PASS PRHASE', passphrase.value)
     // const encryptedFile = await secure.encryptFile(file, passphrase.value);
         const stream = await secure.startStreaming(file, passphrase.value);
 
+console.log('stream returned' , stream)
 
 
-const params = {
-    Bucket: "securesend2", // YOUR_BUCKET_NAME
-    Key: file.name, // Use the file name as the key
-    Body: stream
-  };
-
+const result = await uploadS3(file.name,totalBytes ,stream,1)
+console.log('FINISHED SECURE SEND ECRYPT')
+const endTime = performance.now(); // Capture end time
+    console.log(`SecureSend Took ${endTime - startTime} milliseconds.`);
+    timers.value[0].data.push(parseFloat((endTime - startTime).toFixed(2)))
+// return stream
 
 
 
@@ -264,20 +358,16 @@ const params = {
 
   // }
 
-  const uploader = new Upload({client,params});
+  // const uploader = new Upload({client,params});
   //track prorgress
 
 
 
-uploader.on('httpUploadProgress', (progress) => {
-  awsProgress.value = Math.round((progress.loaded / totalBytes) * 100);
-     console.log('Upload Progress ' ,  awsProgress.value)
-  
-});
+
 
 // console.log("starting upload...")
-  const result = await uploader.done();
-    console.log("Upload completed:", result);
+
+
 //     console.timeEnd('READ>ENCRYPT>UPLOAD')
 
 // const data = await client.send(new PutObjectCommand(command) )
@@ -305,9 +395,34 @@ console.log('ERROR', error)
 
 
 
+const uploadS3 = async (fileName, totalBytes ,stream,index) => {
+  const params = {
+    Bucket: "securesend2", // YOUR_BUCKET_NAME
+    Key: fileName, // Use the file name as the key
+    Body: stream
+  };
+
+  const uploader = new Upload({client,params});
+  uploader.on('httpUploadProgress', (progress) => {
+  awsProgress.value = Math.round((progress.loaded / totalBytes) * 100);
+  radialSeries.value[0] = awsProgress.value
+     console.log('Upload Progress ' ,  awsProgress.value)
+  
+});
+
+
+const result = await uploader.done();
+    console.log("Upload completed:", result);
+ 
+
+return result
+
+}
+
   // // await writable.close();
   // console.timeEnd('startStreaming')
   // progressEmitter.removeEventListener('progress', onProgress);
+
 
 
 
@@ -381,7 +496,7 @@ const decrypt = async () => {
 const listFiles = async () => {
   const { Contents, KeyCount } = await client.send(new ListObjectsV2Command({ Bucket: "securesend2" }));
   data.value = { Contents, KeyCount };
-  console.log('DATA', data.value.Contents)
+  // console.log('DATA', data.value.Contents)
 }
 
 //run listFiles on created
@@ -453,6 +568,11 @@ console.log('DECRYPTED STREAM', decryptedStream)
 
 </script>
 
+<style>
+body {
+  /* background-color:grey; */
+}
+</style>
 
 <style scoped>
 
@@ -460,23 +580,46 @@ console.log('DECRYPTED STREAM', decryptedStream)
   display: flex;
   flex-direction: column;
   gap: 1rem;
-  height: 400px;
-  max-width: 800px;
+  /* height: 400px; */
+  max-width: 1200px;
   margin: auto;
   padding: 1rem;
+  padding-top:0;
+  margin-top:0;
+
+ 
 }
 
 .boxContainer {
   display: flex;
   gap: 1rem;
-width: 100%;
+flex-direction:Row;
+/* align-items:center; */
 justify-content: space-evenly;
+/* border-radius:6px; */
+/* border:1px solid red; */
+
 }
 
 .box {
   /* border: 1px solid red; */
-  padding: 20px;
+  padding: 12px;
+  /* margin:auto; */
+  border-radius:5px;
+display:flex;
+gap:5px;
+width: 420px;
+flex-direction:column;
+align-items:center;
+border:1px solid rgb(230, 230, 230);
+box-shadow: 0px 10px 15px -3px rgba(0,0,0,0.1);
   /* height: 400px; */
   /* width: 400px; */
+}
+
+.box input {
+  padding:7px;
+  border-radius:2px;
+  border:1px solid rgb(129, 129, 129)
 }
 </style>
